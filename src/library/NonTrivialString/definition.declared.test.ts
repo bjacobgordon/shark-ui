4e3324b2b011f8267e4fb792f9bd82d3026d17e6
Jacob Gordon <Jacob.Gordon@amd.com>
2025-12-17 18:05:45 +0000

    wip - tests: scaffolds all object-oriented suites

---
+++ src/library/NonTrivialString/definition.declared.test.ts
@@ -0,0 +1,18 @@
+import {
+  describe,
+} from 'vitest';
+
+import {
+  NonTrivialString,
+} from './definition.declared.ts';
+
+describe(NonTrivialString, () => {
+  describe.todo('instantiation');
+
+  // TODO: state whether instances should resemble some other primitive type or object
+  // e.g. "branded string should still be a string rather than a wrapper object"
+  describe.todo('structural compatibility');
+
+  // TODO: state whether instances should be mutable and, if so, specify in what ways
+  describe.todo('mutability');
+});
